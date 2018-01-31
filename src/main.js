d0485d94d9834c5d0e1dd89d5b15686660c9e162
David Vezzani <david.vezzani@greenseedtech.com>
2018-01-30 18:24:55 -0700

    init

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// Require the main Sass manifest file
+require('./assets/sass/main.scss');
+
+// npm install bulma bulma-extensions node-sass sass-loader --save
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})

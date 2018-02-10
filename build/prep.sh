a165316c9034668eaed6c376a2a3b909f5c64a07
David Vezzani <david_vezzani@yahoo.com>
2018-02-10 12:14:13 -0700

    make files relative to script dir

--- build/prep.sh
+++ build/prep.sh
@@ -5,14 +5,16 @@ if [ "$1" == "" ]; then
   exit 1
 fi
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 if [ "$1" == "dev" ]; then
-  sed -i -e 's#^[[:space:]]*baseURL: [^,]*,$#	baseURL: "http://10.0.0.236:3000/api/",#g' ../src/api.js ../src/App.vue
-  sed -i -e 's#^[[:space:]]*<meta name="google-signin-client_id" content="[^"]*">[[:space:]]*$#		<meta name="google-signin-client_id" content="38977793520-u51b5kpvkfuokp2ev9vb3vllfkl6oaqo.apps.googleusercontent.com">#g' ../index.html
+  sed -i -e 's#^[[:space:]]*baseURL: [^,]*,$#	baseURL: "http://10.0.0.236:3000/api/",#g' $DIR/../src/api.js $DIR/../src/App.vue
+  sed -i -e 's#^[[:space:]]*<meta name="google-signin-client_id" content="[^"]*">[[:space:]]*$#		<meta name="google-signin-client_id" content="38977793520-u51b5kpvkfuokp2ev9vb3vllfkl6oaqo.apps.googleusercontent.com">#g' $DIR/../index.html
 fi
 
 if [ "$1" == "prod" ]; then
-  sed -i -e 's#^[[:space:]]*baseURL: [^,]*,$#	baseURL: "https://reliacode.com/api/",#g' ../src/api.js ../src/App.vue
-  sed -i -e 's#^[[:space:]]*<meta name="google-signin-client_id" content="[^"]*">[[:space:]]*$#		<meta name="google-signin-client_id" content="38977793520-2eute2ed36ddd4kjj6ss91d7vecsukpj.apps.googleusercontent.com">#g' ../index.html
+  sed -i -e 's#^[[:space:]]*baseURL: [^,]*,$#	baseURL: "https://reliacode.com/api/",#g' $DIR/../src/api.js $DIR/../src/App.vue
+  sed -i -e 's#^[[:space:]]*<meta name="google-signin-client_id" content="[^"]*">[[:space:]]*$#		<meta name="google-signin-client_id" content="38977793520-2eute2ed36ddd4kjj6ss91d7vecsukpj.apps.googleusercontent.com">#g' $DIR/../index.html
 fi
 
 
